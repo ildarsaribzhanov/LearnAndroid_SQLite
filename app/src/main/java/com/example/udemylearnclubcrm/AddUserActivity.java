fd3579259113c1f4576e717ad08469fa3dab542e
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-28 15:25:41 +0300

    [Add] processing change gender value

--- app/src/main/java/com/example/udemylearnclubcrm/AddUserActivity.java
+++ app/src/main/java/com/example/udemylearnclubcrm/AddUserActivity.java
@@ -3,6 +3,9 @@ package com.example.udemylearnclubcrm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -33,5 +36,35 @@ public class AddUserActivity extends AppCompatActivity {
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         genderSpinner.setAdapter(genderAdapter);
+
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = (String) parent.getItemAtPosition(position);
+
+                if (TextUtils.isEmpty(selected)) {
+                    return;
+                }
+
+                switch (selected) {
+                    case "Male":
+                        gender = 1;
+                        break;
+
+                    case "Female":
+                        gender = 2;
+                        break;
+
+                    default:
+                        gender = 0;
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                gender = 0;
+            }
+        });
     }
 }
