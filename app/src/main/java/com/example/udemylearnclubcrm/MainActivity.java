897d8e6ea54de2a43c25c573439153d8818dc3cd
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-28 14:51:40 +0300

    [Add] button

--- app/src/main/java/com/example/udemylearnclubcrm/MainActivity.java
+++ app/src/main/java/com/example/udemylearnclubcrm/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addUserBtn = findViewById(R.id.floatingActionButton);
     }
 }
